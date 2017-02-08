b1563554542748b80201fe46f5c4f70c7db094f5
edwin putra <edwin putra>
2017-02-08 16:38:17 +0700

    first commit

--- OOP-review/07_polymorphism/interface/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require 'Jeruk.php';
-
-$jeruk = new Jeruk();
-$jeruk->ambilWarna(); // Hasil : 'Warna Jeruk adalah Orange'
--- OOP-review/11_scope_resolution_operator/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once 'Handphone.php';
-
-var_dump(Handphone::$jenisHandphone);
-var_dump(Handphone::cetakJenisHandphone());
